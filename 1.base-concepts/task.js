a7bdbc5b2516f4a8c48ad6e879225e6e25b82b23
Olga Gogoleva <mikadoolga@yandex.ru>
2025-01-14 21:50:06 +0300

    solution to lecture 1

--- 1.base-concepts/task.js
+++ 1.base-concepts/task.js
@@ -1,10 +1,31 @@
 "use strict"
 function solveEquation(a, b, c) {
   let arr = [];
+  let discriminant = b ** 2 - 4 * a * c;
+  let d = discriminant;
   
-  return arr;
+  if (discriminant < 0) { //Если дискриминант меньше нуля, то корней нет (пустой массив)
+      return arr;
+  } else if (discriminant === 0) { //Если дискриминант равен нулю, возвращаем массив с одним элементом
+      let root = -b / (2 * a);
+      return [root];  
+  } else {  //Если дискриминант больше нуля -возвращаем массив с двумя элементами
+    return [
+			(-b + Math.sqrt(d)) / (2 * a),  
+			(-b - Math.sqrt(d)) / (2 * a)
+		]  
+  }
 }
 
-function calculateTotalMortgage(percent, contribution, amount, countMonths) {
-  
-}
+function calculateTotalMortgage(percent, contribution, amount, countMonths){
+
+  let P = percent/ 100 / 12; //Преобразование процентной ставки
+  let S = amount - contribution; //Тело кредита — сумму, которую нужно вернуть банку
+  let n = countMonths ;
+ 
+  let monthlyPayment = (S * (P + (P / (Math.pow ((1 + P),n) - 1)))); //общая сумма, которую придётся заплатить клиенту
+ 
+  let totalAmount = (monthlyPayment * n).toFixed(2); //Округлите результат до двух значений после запятой
+ 
+ return Number(totalAmount);
+ }
